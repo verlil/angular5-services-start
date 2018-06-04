86f801f10cfacd489e4c37986921be4d6dd42086
verlil <vernigoralilia@gmail.com>
2018-06-04 04:11:54 +0300

    LoggingService

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,18 +1,25 @@
 import { Component, EventEmitter, Output } from '@angular/core';
+import { LoggingService } from '../shared/logging.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
-  styleUrls: ['./new-account.component.css']
+  styleUrls: ['./new-account.component.css'],
+  providers: [LoggingService]
+  //providers: [LoggingService]  - this tels Angular how to provide(create) an instance of LoggingService
 })
 export class NewAccountComponent {
   @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
+
+  constructor (private loggingService: LoggingService){
+    //private loggingService: LoggingService  - this informs  we need an instance of LoggingService
+  }
 
   onCreateAccount(accountName: string, accountStatus: string) {
     this.accountAdded.emit({
       name: accountName,
       status: accountStatus
     });
-    console.log('A server status changed, new status: ' + accountStatus);
+    this.loggingService.logStatusChange(accountStatus);
   }
 }
