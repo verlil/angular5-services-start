3e9bb824bf849ecdc0420009baebd114091507e2
verlil <vernigoralilia@gmail.com>
2018-06-04 04:35:05 +0300

    AccountsService provided withouth hirerarchical injection

--- src/app/new-account/new-account.component.ts
+++ src/app/new-account/new-account.component.ts
@@ -1,25 +1,22 @@
-import { Component, EventEmitter, Output } from '@angular/core';
+import { Component } from '@angular/core';
 import { LoggingService } from '../shared/logging.service';
+import { AccountsService } from '../shared/accounts.service';
 
 @Component({
   selector: 'app-new-account',
   templateUrl: './new-account.component.html',
   styleUrls: ['./new-account.component.css'],
-  providers: [LoggingService]
+  providers: [LoggingService, AccountsService]
   //providers: [LoggingService]  - this tels Angular how to provide(create) an instance of LoggingService
 })
 export class NewAccountComponent {
-  @Output() accountAdded = new EventEmitter<{name: string, status: string}>();
 
-  constructor (private loggingService: LoggingService){
+  constructor (private loggingService: LoggingService, private accountsService: AccountsService){
     //private loggingService: LoggingService  - this informs  we need an instance of LoggingService
   }
 
   onCreateAccount(accountName: string, accountStatus: string) {
-    this.accountAdded.emit({
-      name: accountName,
-      status: accountStatus
-    });
+    this.accountsService.addAccount(accountName, accountStatus)
     this.loggingService.logStatusChange(accountStatus);
   }
 }
--- src/app/shared/logging.service.ts
+++ src/app/shared/logging.service.ts
@@ -1,8 +1,4 @@
 //service - is just a normal TS class
-import { Injectable } from '@angular/core';
-
-@Injectable()
-
 export class LoggingService {
   logStatusChange(status: string){
     console.log('A server status changed, new status: ' + status);
